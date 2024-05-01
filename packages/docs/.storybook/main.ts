56674b4f1ee5e12a8bc077f13251d08c01be9b24
felipemaifredo <felipemaifredo@gmail.com>
2024-05-01 06:29:16 -0300

    att 2

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -26,5 +26,11 @@ const config: StorybookConfig = {
   docs: {
     autodocs: true,
   },
+  viteFinal: (config, {configType}) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/design-system-rocketseat'
+    }
+    return config
+  }
 };
 export default config;
